86d96ff267a49d7245afa278c55665dee2d6579e
AidiRi <rivera.aidi@gmail.com>
2019-08-10 11:30:01 -0700

    Created BaseApp component. App.js will be used like index.js

--- baseApp.js
+++ baseApp.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import AppNavigator from './navigation/AppNavigator';
+
 
 class BaseApp extends Component {
 
   render () {
     return(
-
+      <AppNavigator />
 
     )
   }
 }
 
-export default BaseApp; 
+export default BaseApp;
