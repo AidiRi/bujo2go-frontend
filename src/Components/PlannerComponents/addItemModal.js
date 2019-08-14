f074fe71caab674a36a9203c64c459225e258ab9
AidiRi <rivera.aidi@gmail.com>
2019-08-14 10:39:11 -0700

    Added WORKING basic modal

--- src/Components/PlannerComponents/addItemModal.js
+++ src/Components/PlannerComponents/addItemModal.js
@@ -1,34 +1,39 @@
-import React, { PureComponent } from 'react'
-import { Text, TouchableHighlight, View, Alert, StyleSheet, Button, Modal} from 'react-native'
-// import Modal from "react-native-modal";
+import React, { PureComponent, useState } from 'react'
+import { Text, TouchableHighlight, View, Alert, StyleSheet, Button} from 'react-native'
+import Modal from "react-native-modal";
 
 
 // props={
 // isAddModalDislay,
 // toggleAddModal() }
-class AddItemModal extends PureComponent{
+const AddItemModal = props => {
 
-  state = {
-    isModalVisible: false
-  }
+const [ isVisible, setIsVisible] = useState(false)
+//   constructor(){
+//     super()
+//     this.state = {
+//       isModalVisible: false
+//     }
+// }
+  //
+  // toggleModal = () => {
+  //   this.setState({ isModalVisible: !this.state.isModalVisible });
+  // };
 
-  toggleModal = () => {
-    this.setState({ isModalVisible: !this.state.isModalVisible });
-  };
+  const toggleModal = () => setIsVisible( isVisible ? false : true )
 
-  render() {
-    if (!this.props.isAddModalOpen){
+
     return(
       <View style={{ flex: 1 }}>
-        <Button title="Show modal" onPress={this.toggleModal} />
-        <Modal visible={this.state.isModalVisible}>
+        <Button title="Show modal" onPress={toggleModal} />
+        <Modal isVisible={isVisible}>
+          <View style={{ flex: 1 }}>
             <Text>Hello!</Text>
-
+            <Button title="Hide modal" onPress={toggleModal} />
+          </View>
         </Modal>
       </View>
     )
-  }
-  }
 }
 
 const styles = StyleSheet.create({
